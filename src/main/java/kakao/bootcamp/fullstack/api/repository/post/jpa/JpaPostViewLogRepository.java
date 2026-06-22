cda54a83e72a605185b184ae7c2fbb38ee9af81e
LeeDongHoon <likelion.leedh@gmail.com>
2026-06-22 20:44:59 +0900

    refactor : PostViewLog 엔티티 조회 JPQL 작성

--- src/main/java/kakao/bootcamp/fullstack/api/repository/post/jpa/JpaPostViewLogRepository.java
+++ src/main/java/kakao/bootcamp/fullstack/api/repository/post/jpa/JpaPostViewLogRepository.java
@@ -3,8 +3,11 @@ package kakao.bootcamp.fullstack.api.repository.post.jpa;
 import java.util.Optional;
 import kakao.bootcamp.fullstack.api.domain.post.PostViewLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface JpaPostViewLogRepository extends JpaRepository<PostViewLog, Long> {
 
-    Optional<PostViewLog> findByPostIdAndMemberId(Long postId, Long memberId);
+    @Query("SELECT pvl FROM PostViewLog pvl WHERE pvl.memberId = :memberId AND pvl.postId = :postId")
+    Optional<PostViewLog> findByPostIdAndMemberId(@Param("postId") Long postId, @Param("memberId")Long memberId);
 }
