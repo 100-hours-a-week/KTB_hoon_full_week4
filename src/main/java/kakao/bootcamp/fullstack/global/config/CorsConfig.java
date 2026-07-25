61ae6bf0d2045a1198ef752958a070633eb2c683
LeeDongHoon <likelion.leedh@gmail.com>
2026-07-25 19:38:46 +0900

    fix : 배포 주소 CORS 추가

--- src/main/java/kakao/bootcamp/fullstack/global/config/CorsConfig.java
+++ src/main/java/kakao/bootcamp/fullstack/global/config/CorsConfig.java
@@ -14,6 +14,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOrigin("http://localhost:5173");
         config.addAllowedOrigin("http://127.0.0.1:5173");
+        config.addAllowedOrigin("http://3.34.181.250");
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
